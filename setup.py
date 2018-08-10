8db795cc2c0f0968c1389799e633ba337a65aa7d
Jared McKnight <jmcknight@archertech.ca>
2018-08-09 20:58:10 -0400

    updates version, ignores some files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='fl33t-client',
-    version='0.1',
+    version='0.2.1',
     description='Fl33t API Client',
     url='https://github.com/fictivekin/fl33t-client',
     author='Fictive Kin LLC',
