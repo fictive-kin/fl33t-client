da4a7c8191863663edc1145e5930d2b217b3a086
Jared McKnight <jmcknight@archertech.ca>
2018-08-14 16:03:18 -0400

    use the MIT license

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ setup(
     name='fl33t',
     version='0.3.2',
     description='Fl33t API Client',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
     url='https://github.com/fictivekin/fl33t-client',
     author='Fictive Kin LLC',
     author_email='hello@fictivekin.com',
     maintainer='Fictive Kin LLC',
     maintainer_email='systems@fictivekin.com',
+    license='MIT',
     packages=['fl33t'],
     install_requires=[
         'python-dateutil',
