44ff9b3108ddcde76a1ee111ca3e430875f5a6ed
Jared McKnight <jmcknight@archertech.ca>
2019-04-01 13:30:28 -0400

    add 3.7 as supported python version; enable fl33t.cli as package

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ classifiers = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Software Development',
 ]
 
@@ -37,6 +38,7 @@ if __name__ == "__main__":
         python_requires=">=3.4",
         packages=[
             'fl33t',
+            'fl33t.cli'
             'fl33t.models'
         ],
         install_requires=[
